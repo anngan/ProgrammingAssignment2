6be1fe20b72096bfb68c577c8103fd626e84cb62
anngan <anna.gandziarska@gmail.com>
2017-06-01 10:46:34 +0100

    edit cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,15 @@
 ## R Programming Assignment 2: Lexical Scoping
-## Matrix inversion is usually a costly computation and it can be beneficial to find a way of caching the inverse of a matrix  
-## instead of computing it repeatedly. In order to make it easier, two functions can be used. 
+## Matrix inversion is usually a costly computation and it can be beneficial to find a way of 
+## caching the inverse of a matrix instead of computing it repeatedly.
+## In order to make it easier, two functions can be used. 
 
-## Firstly, function makeCacheMatrix can be utilized: this function creates a special 'matrix' object that can cache
-## its inverse. This special 'matrix' object is a list that includes functions that set and get the value of the matrix 
-## and set and get the value of the inverse of the matrix.
+## First function is makeCacheMatrix: this function creates a special 'matrix'
+## object that can cache its inverse. This special 'matrix' object is a list that includes
+## functions that set and get the value of the matrix and set and get the value of the inverse
+## of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-        inv <- NULL
+        inv <- NULL 
         set <- function(y) {
                 x <<- y
                 inv <<- NULL
@@ -20,9 +22,10 @@ makeCacheMatrix <- function(x = matrix()) {
              setInv = setInv, getInv = getInv)
 }
 
-## Secondly, cacheSolve function can be used. cacheSolve function computes the inverse of the special matrix that is returned 
-## by the function makeCacheMatrix. In the case that the inverse has been already computed and the matrix remained the same, 
-## it should retrieve the inverse from the created cache.
+## Secondly, cacheSolve function can be used. cacheSolve function computes the inverse  
+## of the special matrix that is returned by the function makeCacheMatrix. In the case that
+## the inverse has been already computed and the matrix remained the same, it should
+## retrieve the inverse from the created cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
